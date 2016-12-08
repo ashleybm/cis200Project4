666ca306a345a3c16033a6b16099212aae368d86
ShijakuEgil <eshijaku@umich.edu>
2016-12-08 10:19:50 -0500

    Add files via upload
    A lot of the code we are working on

--- Source.cpp
+++ Source.cpp
@@ -4,12 +4,17 @@ program 4 CIS 200
 */
 
 #include<iostream>
+#include<string>
 #include<stack>
 #include<queue>
 using namespace std;
 
-const int LOADTIME = 2;
-class Train {
+const int TRAIN_LOAD_TIME = 2;
+const int PLANE_LOAD_TIME = 10;
+
+
+class Train 
+{
 	int trainNr;
 	int maxNrPackages;
 	int currNrPackages;
@@ -20,13 +25,18 @@ public:
 		trainNr = nr;
 		maxNrPackages = nrPack;
 	}
-	void load()
+	void load(int docTime)
 	{
-
+		time = docTime + ((trainNr * TRAIN_LOAD_TIME) / 2);//divided by 2 because this is only half way.
+														//There is still the trip back to the dock.
 	}
-
+	int getTime()
+	{
+		return time;
+	}
 };
-class Plane {
+class Plane 
+{
 	int planeNr;
 	int maxNrPackages;
 	int currNrPackages;
@@ -37,34 +47,44 @@ public:
 		planeNr = nr;
 		maxNrPackages = nrPack;
 	}
-	void load() 
+
+	void load(int docTime) 
 	{
-
+		time = docTime + ((planeNr * PLANE_LOAD_TIME) / 2);//divided by 2 because this is only half way.
+														//There is still the trip back to the dock.
 	}
-
+	int getTime()
+	{
+		return time;
+	}
 };
 
-class Dock {
-	stack<int> tempStack;
-	stack<int> stackOf5;
-	queue<int> assemblyLine;
+class excededTotalNubmerOfPackages
+{
+	string message;
+public: 
+	excededTotalNubmerOfPackages(int maxPack, string type) {
+		message = " Exceded maximum number of packages for " + type + ".\n" +
+			+" Maximum number is: " + to_string(maxPack) + "\n";
+	}
+	void error()
+	{
+		cout << message;
+	}
+};
+class noSuchIdException
+{
+	string message;
 public:
-	Dock(stack<int> s, queue<int> q)
+	noSuchIdException(string type, int pckId)
 	{
-		while (!s.empty())
-		{
-			tempStack.push(s.top());
-			s.pop();
-		}
-		while (!q.empty())
-		{
-			assemblyLine.push(q.front());
-			q.pop();
-		}
+		message = type + " number " + to_string(pckId) + " is not in dock\n";
 	}
-
+	void error()
+	{
+		cout << message;
+	}
 };
-
 int main()
 {
 	int nrTrains;
@@ -72,38 +92,112 @@ int main()
 	int trainTotalPackages;
 	int planeTotalPackages;
 	int nrPackage;
+	int packageId;
 	Train ** trains;
 	Plane ** planes;
 	cin >> nrTrains >> nrPlanes >> trainTotalPackages >> planeTotalPackages;
 
 	trains = new Train *[nrTrains];
 	planes = new Plane *[nrPlanes];
+	int tempNrOfPackages = 0;
 	for (int i = 1; i <= nrTrains; i++)
 	{
 		cin >> nrPackage;
+		tempNrOfPackages = tempNrOfPackages + nrPackage;
+		try
+		{
+			if (tempNrOfPackages > trainTotalPackages)
+				throw excededTotalNubmerOfPackages(trainTotalPackages, "trains");
+		}
+		catch(excededTotalNubmerOfPackages e)
+		{
+			e.error();
+			cin >> nrPackage;
+			tempNrOfPackages = tempNrOfPackages + nrPackage;
+		}
 		trains[i] = new Train(i, nrPackage);
 	}
+	tempNrOfPackages = 0;
 	for (int i = 1; i <= nrPlanes; i++)
 	{
+
 		cin >> nrPackage;
+		tempNrOfPackages = tempNrOfPackages + nrPackage;
+		try
+		{
+			if (tempNrOfPackages > planeTotalPackages)
+				throw excededTotalNubmerOfPackages(planeTotalPackages, "planes");
+		}
+		catch (excededTotalNubmerOfPackages e)
+		{
+			e.error();
+		}
 		planes[i] = new Plane(i, nrPackage);
 		
 	}
-	stack<int> stack;
-	queue<int> queue;
+	queue<int> tempTrainQueue;
+	queue<int> convBelt;
+	stack<int> stackOf5;
 	for (int i = 0; i < trainTotalPackages; i++)
 	{
-		cin >> nrPackage;
-		stack.push(nrPackage);
-	}
-	for (int i = 0; i < planeTotalPackages; i++)
-	{
-		cin >> nrPackage;
-		queue.push(nrPackage);
+		cin >> packageId; 
+		try {
+			if (packageId > nrTrains || packageId <= 0)
+				throw noSuchIdException("Train", packageId);
+		}
+		catch (noSuchIdException e) {
+			e.error();
+		}
+
+		tempTrainQueue.push(packageId);
 	}
 
-	Dock dock(stack, queue);
-	
+	for (int i = 0; i < planeTotalPackages; i++)
+	{
+		cin >> packageId;
+		try {
+			if (packageId > nrPlanes || packageId <= 0)
+				throw noSuchIdException("Plane", packageId);
+		}
+		catch (noSuchIdException e) {
+			e.error();
+		}
+		convBelt.push(packageId);
+	}
+	int dockTrainTime = 0;
+
+	while (!tempTrainQueue.empty())
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			stackOf5.push(tempTrainQueue.front());
+			tempTrainQueue.pop();
+		}
+		while (!stackOf5.empty())
+		{
+			trains[stackOf5.top()]->load(dockTrainTime);
+			dockTrainTime = dockTrainTime + (stackOf5.top() * TRAIN_LOAD_TIME);
+			stackOf5.pop();
+		}
+	}
+	int dockPlaneTime = 0;
+	for (int i = 1; i <= nrTrains; i++)
+	{
+		cout << trains[i]->getTime() << " ";
+	}
+	cout << endl;
+
+	while (!convBelt.empty())
+	{
+		planes[convBelt.front()]->load(dockPlaneTime);
+		dockPlaneTime = dockPlaneTime + (convBelt.front() * PLANE_LOAD_TIME);
+		convBelt.pop();
+	}
+	for (int i = 1; i <= nrPlanes; i++)
+	{
+		cout << planes[i]->getTime() << " ";
+	}
+	cout << endl;
 	return 0;
 }
 
